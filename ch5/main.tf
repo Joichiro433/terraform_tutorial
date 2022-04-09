2cfef2b23ed4ce77c1bd3722c8b6718a12fb7ff6
Joichiro433 <joichiro322@gmail.com>
2022-04-09 23:26:30 +0900

    update: 不要なコメントを削除

--- ch5/main.tf
+++ ch5/main.tf
@@ -99,12 +99,6 @@ resource "aws_vpc" "example" {
   }
 }
 
-# resource "aws_subnet" "public" {
-#   vpc_id                  = aws_vpc.example.id
-#   cidr_block              = "10.0.0.0/24"
-#   map_public_ip_on_launch = true
-#   availability_zone       = "ap-northeast-1a"
-# }
 resource "aws_subnet" "public_0" {
   vpc_id                  = aws_vpc.example.id
   cidr_block              = "10.0.1.0/24"
@@ -133,10 +127,6 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-# resource "aws_route_table_association" "public" {
-#   subnet_id      = aws_subnet.public.id
-#   route_table_id = aws_route_table.public.id
-# }
 resource "aws_route_table_association" "public_0" {
   subnet_id      = aws_subnet.public_0.id
   route_table_id = aws_route_table.public.id
@@ -147,12 +137,6 @@ resource "aws_route_table_association" "public_1" {
   route_table_id = aws_route_table.public.id
 }
 
-# resource "aws_subnet" "private" {
-#   vpc_id                  = aws_vpc.example.id
-#   cidr_block              = "10.0.64.0/24"
-#   availability_zone       = "ap-northeast-1a"
-#   map_public_ip_on_launch = false
-# }
 resource "aws_subnet" "private_0" {
   vpc_id                  = aws_vpc.example.id
   cidr_block              = "10.0.65.0/24"
@@ -166,21 +150,6 @@ resource "aws_subnet" "private_1" {
   availability_zone       = "ap-northeast-1c"
   map_public_ip_on_launch = false
 }
-
-# resource "aws_route_table" "private" {
-#   vpc_id = aws_vpc.example.id
-# }
-
-# resource "aws_route_table_association" "private" {
-#   subnet_id      = aws_subnet.private.id
-#   route_table_id = aws_route_table.private.id
-# }
-
-# resource "aws_route" "private" {
-#   route_table_id         = aws_route_table.private.id
-#   nat_gateway_id         = aws_nat_gateway.example.id
-#   destination_cidr_block = "0.0.0.0/0"
-# }
 
 resource "aws_route_table" "private_0" {
   vpc_id = aws_vpc.example.id
@@ -211,17 +180,6 @@ resource "aws_route_table_association" "private_1" {
   subnet_id      = aws_subnet.private_1.id
   route_table_id = aws_route_table.private_1.id
 }
-
-# resource "aws_eip" "nat_gateway" {
-#   vpc        = true
-#   depends_on = [aws_internet_gateway.example]
-# }
-
-# resource "aws_nat_gateway" "example" {
-#   allocation_id = aws_eip.nat_gateway.id
-#   subnet_id     = aws_subnet.public.id
-#   depends_on    = [aws_internet_gateway.example]
-# }
 
 resource "aws_eip" "nat_gateway_0" {
   vpc        = true
